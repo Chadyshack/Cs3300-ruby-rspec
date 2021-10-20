882c9a94ca56697e2973fe43d25f678a295ebb03
Chad Schwenke <cschwenk@uccs.edu>
2021-10-20 02:35:15 +0000

    Part 1 complete

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,15 +3,32 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  if arr.length == 0
+    return 0
+  end
+  if arr.length == 1
+    return arr[0]
+  end
+  first = arr.max(2)
+  return first[0] + first[1]
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  if arr.length == 1 || arr.length == 0
+    return false
+  end
+  for i in arr
+    for j in arr
+      if i != j && (i + j) == n
+        return true
+      end
+    end
+  end
+  return false
 end
 
 # Part 2
